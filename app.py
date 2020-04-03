ca991ececfb885421d2ea4d99ccf575e77254f4e
Zixuan Zhang <zhan7230@umn.edu>
2020-04-03 18:40:58 -0500

    Add signup functionality

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 """Flask Application for Paws Rescue Center."""
 from flask import Flask, render_template, abort
-from forms import LoginForm
+from forms import LoginForm, SignUpForm
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'mysecret'
+app.config['SECRET_KEY'] = 'dfewfew123213rwdsgert34tgfd1234trgf'
 
 """Information regarding the Pets in the System."""
 pets = [
@@ -12,6 +12,11 @@ pets = [
             {"id": 2, "name": "Yuki", "age": "8 months", "bio": "I am a handsome gentle-cat. I like to dress up in bow ties."},
             {"id": 3, "name": "Basker", "age": "1 year", "bio": "I love barking. But, I love my friends more."},
             {"id": 4, "name": "Mr. Furrkins", "age": "5 years", "bio": "Probably napping."}, 
+        ]
+
+"""Information regarding the Users in the System."""
+users = [
+            {"id": 1, "full_name": "Pet Rescue Team", "email": "team@pawsrescue.co", "password": "adminpass"},
         ]
 
 @app.route("/")
@@ -44,6 +49,19 @@ def login():
         print(form.password.errors)
 
     return render_template("login.html", form = form)
+
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
+    """View function for Showing Details of Each Pet.""" 
+    form = SignUpForm()
+    if form.validate_on_submit():
+        new_user = {'id': len(users) + 1, 
+                    'full_name': form.full_name.data, 
+                    'email': form.email.data, 
+                    'password': form.password.data}
+        users.append(new_user)
+        return render_template('signup.html', message='Successfully signed up')
+    return render_template("signup.html", form = form)
 
 if __name__ == '__main__':
     app.run(debug=True)
