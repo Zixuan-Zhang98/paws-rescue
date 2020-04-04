da4e4487a6e7319e027d42dc67eae46897a8af7f
Zixuan Zhang <zhan7230@umn.edu>
2020-04-03 19:13:39 -0500

    Add user login/logout functionality

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 """Flask Application for Paws Rescue Center."""
 from flask import Flask, render_template, abort
-from forms import LoginForm, SignUpForm
+from forms import SignUpForm, LoginForm
+from flask import session, redirect, url_for
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'dfewfew123213rwdsgert34tgfd1234trgf'
 
 """Information regarding the Pets in the System."""
@@ -19,15 +19,18 @@ users = [
             {"id": 1, "full_name": "Pet Rescue Team", "email": "team@pawsrescue.co", "password": "adminpass"},
         ]
 
+
 @app.route("/")
 def homepage():
     """View function for Home Page."""
     return render_template("home.html", pets = pets)
 
+
 @app.route("/about")
 def about():
     """View function for About Page."""
     return render_template("about.html")
+
 
 @app.route("/details/<int:pet_id>")
 def pet_details(pet_id):
@@ -37,31 +40,35 @@ def pet_details(pet_id):
         abort(404, description="No Pet was Found with the given ID")
     return render_template("details.html", pet = pet)
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    form = LoginForm()
-
-    if form.validate_on_submit():
-       print("Submitted and Valid.")
-    elif form.errors:
-        print(form.errors.items())
-        print(form.email.errors)
-        print(form.password.errors)
-
-    return render_template("login.html", form = form)
 
 @app.route("/signup", methods=["POST", "GET"])
 def signup():
     """View function for Showing Details of Each Pet.""" 
     form = SignUpForm()
     if form.validate_on_submit():
-        new_user = {'id': len(users) + 1, 
-                    'full_name': form.full_name.data, 
-                    'email': form.email.data, 
-                    'password': form.password.data}
+        new_user = {"id": len(users)+1, "full_name": form.full_name.data, "email": form.email.data, "password": form.password.data}
         users.append(new_user)
-        return render_template('signup.html', message='Successfully signed up')
+        return render_template("signup.html", message = "Successfully signed up")
     return render_template("signup.html", form = form)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = next((user for user in users if user["email"] == form.email.data and user["password"] == form.password.data), None)
+        if user is None:
+            return render_template("login.html", form = form, message = "Wrong Credentials. Please Try Again.")
+        else:
+            session['user'] = user
+            return render_template("login.html", message = "Successfully Logged In!")
+    return render_template("login.html", form = form)
+
+@app.route("/logout")
+def logout():
+    if 'user' in session:
+        session.pop('user')
+    return redirect(url_for('homepage', _scheme='https', _external=True))
+    
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=3000)
